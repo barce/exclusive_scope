0ef251e2a3bd519f4bf7a2616ac0133526f3365f
Barce <barce@me.com>
2014-06-11 00:32:33 -0700

    adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExclusiveScope::Application.config.secret_key_base = 'f77beedea72a5954deb51d73ab0fe6e64bc9492374d6329a1e10fe355d3dd0250109fc72f90f905a69a4e369ba8dc45e6758d51e679a44f9b8a3ee024b61c774'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExclusiveScope::Application.config.session_store :cookie_store, key: '_exclusive_scope_session'
