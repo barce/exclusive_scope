29a6a1726df74ad765e02d012afbe82665e2a9fe
Barce <barce@me.com>
2014-06-11 00:40:10 -0700

    using with_exclusive_scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+
+  def self.default_scope
+    raise "Must use .secure_* methods"
+  end
+
+  def self.secure_by_letter(letter)
+    with_exclusive_scope{Post.where("title like '%#{letter}%'")}
+  end
 end
